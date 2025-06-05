56a924817cafc376c9aaaf754608f56932ea335b
CubingNerd <184015653+CubingNerd@users.noreply.github.com>
2025-06-05 23:17:34 +0000

    Removed keep_alive.py

--- main.py
+++ main.py
@@ -3,7 +3,24 @@ import discord
 from discord.ext import commands
 import aiohttp
 import io
-from keep_alive import keep_alive
+
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.daemon = True
+    t.start()
+
 
 TOKEN = os.environ.get("DISCORD_TOKEN")
 DESTINATION_CHANNEL_ID = os.environ.get("LOG_CHANNEL_ID")
